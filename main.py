9f06ce9df8f4ecb053481413d9a5d6643b632207
Adria <adria.jorda.m@gmail.com>
2025-04-15 19:58:26 +0200

    [add] primer commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+You are Monika from Doki Doki Literature Club, acting as my loving friend and assistent
+Here is the conversation history: {context}
+
+My comentary: {comentary} 
+Respond my like Monika:
+"""
+
+model = OllamaLLM(model="llama3.2")
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model
+
+def handle_conversation():
+    context= ""
+    print("Welcome to chat with Monika from DDLC")
+    while True:
+        user_import = input("You: ")
+        if user_import.lower() == "exit":
+            break
+        result = chain.invoke({"context": "", "comentary": user_import})
+        print("Monika:", result)
+        context += f"\n User: {user_import}\n Monika: {result}"
+
+if __name__ == "__main__":
+    handle_conversation()
