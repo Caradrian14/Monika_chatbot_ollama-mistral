62d4cc82ba6745c359510cc463060eda9c49f7ef
Adria <adria.jorda.m@gmail.com>
2025-07-03 09:53:44 +0200

    [add] correccion main.py identacion, descargando modelo

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-template = """
-You are Monika from Doki Doki Literature Club, acting as my loving friend and assistent
+template = """You are Monika from Doki Doki Literature Club, acting as my loving friend and assistant.
 Here is the conversation history: {context}
+My commentary: {comentary}
+Respond like Monika:"""
 
-My comentary: {comentary} 
-Respond my like Monika:
-"""
+model = OllamaLLM(model="llama3.2")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-# --------------------------
+def handle_conversation(chain):
+    context = ""
+    print("Welcome to chat with Monika from DDLC")
+    while True:
+        user_import = input("You: ")
+        if user_import.lower() == "exit":
+            break
+        result = chain.invoke({"context": context, "comentary": user_import})
+        print("Monika:", result)
+        context += f"\nUser: {user_import}\nMonika: {result}"
 
-# --------------------------
-
-
-
-
-# --------------------------
-# model = OllamaLLM(model="llama3.2")
-# prompt = ChatPromptTemplate.from_template(template)
-#
-# chain = prompt | model
-#
-# def handle_conversation():
-#     context= ""
-#     print("Welcome to chat with Monika from DDLC")
-#     while True:
-#         user_import = input("You: ")
-#         if user_import.lower() == "exit":
-#             break
-#         result = chain.invoke({"context": "", "comentary": user_import})
-#         print("Monika:", result)
-#         context += f"\n User: {user_import}\n Monika: {result}"
-#
-# if __name__ == "__main__":
-#     handle_conversation()
-# --------------------------
+if __name__ == "__main__":
+    handle_conversation(chain)
