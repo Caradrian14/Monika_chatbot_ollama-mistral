8e937ee48dba075b66766bcc18c12e430845daee
Adria <adria.jorda.m@gmail.com>
2025-07-04 09:09:37 +0200

    [add] cambiado la estructura visual

--- app_view.py
+++ app_view.py
@@ -7,10 +7,20 @@ ventana.title("Monika Assistant")
 
 # Tamaño de la ventana
 ventana.geometry("500x600")
+ventana.attributes('-alpha', 0.95)
+# Configurar colores para el tema oscuro
+color_fondo = "#2d2d2d"
+color_texto = "#ffffff"
+
+
 
 # Área de texto para el historial del chat
-historial_chat = tk.Text(ventana, state='normal', width=60, height=30)
+historial_chat = tk.Text(ventana, state='normal', width=60, height=30, bg=color_fondo, fg=color_texto, insertbackground=color_texto)
 historial_chat.pack(padx=10, pady=10)
+
+# Campo de entrada para nuevos mensajes
+entrada_texto = tk.Entry(ventana, width=50, bg=color_fondo, fg=color_texto, insertbackground=color_texto)
+entrada_texto.pack(padx=10, pady=5)
 
 # -------------- AI
 template = """You are Monika from Doki Doki Literature Club, acting as my loving friend and assistant.
@@ -38,8 +48,8 @@ def enviar_mensaje(chain):
         historial_chat.see(tk.END)
 
 # Campo de entrada para nuevos mensajes
-entrada_texto = tk.Entry(ventana, width=50)
-entrada_texto.pack(padx=10, pady=5)
+# entrada_texto = tk.Entry(ventana, width=50)
+# entrada_texto.pack(padx=10, pady=5)
 
 # Botón para enviar mensajes
 boton_enviar = tk.Button(ventana, text="Enviar", command=enviar_mensaje)
