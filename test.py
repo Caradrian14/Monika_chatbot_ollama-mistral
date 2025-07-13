226aa6e5c52beec5aab1791f125770bac59313f2
Adria <adria.jorda.m@gmail.com>
2025-07-13 22:37:33 +0200

    [test] ventanas laterales

--- test.py
+++ test.py
@@ -16,26 +16,14 @@ window = tk.Tk()
 title_window = os.getenv('WINDOW_TITLE')
 window.title(title_window)
 
-# Crear un Frame principal para el chat
-main_frame = tk.Frame(window)
-main_frame.pack(fill=tk.BOTH, expand=True)
-
+# Window Size
+windows_geometry = os.getenv('WINDOW_SIZE')
+window.geometry(windows_geometry)
+window.attributes('-alpha', 0.95)
 # Colors for the background
 background_color = os.getenv('CHAT_BACKGROUND_COLOR')
 color_text = os.getenv('CHAT_COLOR_TEXT')
 
-# Crear un Canvas y una Scrollbar
-canvas_frame = tk.Canvas(main_frame, bg=background_color)
-chat_scrollbar = ttk.Scrollbar(main_frame, orient="vertical", command=canvas_frame.yview)
-canvas_frame.configure(yscrollcommand=chat_scrollbar.set)
-
-# Empaquetar el Canvas y la Scrollbar
-chat_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-canvas_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-# Crear un Frame interno dentro del Canvas para contener los mensajes
-#inner_frame = tk.Frame(canvas_frame, bg=background_color)
-#canvas_frame.create_window((0, 0), window=inner_frame, anchor=tk.NW)
 
 try:
     user_icon = tk.PhotoImage(file=os.getenv('CHAT_USER_ICON'))
@@ -59,37 +47,6 @@ model_name = os.getenv('MODEL_OLLAMA')
 model = OllamaLLM(model=model_name)
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
-
-# --------------
-def show_message(autor, mensaje, icon=None):
-    try:
-        # Crear un Frame para el mensaje
-        frame = tk.Frame(canvas_frame, bg=background_color)
-        frame.grid_columnconfigure(0, weight=1)
-        frame.grid_columnconfigure(1, weight=1)
-
-        if icon:
-            tag_icon = tk.Label(frame, image=icon, bg=background_color)
-            tag_icon.grid(row=0, column=0, sticky='e', padx=(0, 5))
-
-        tag_message = tk.Label(frame, text=f"{autor}: {mensaje}", bg=background_color, fg=color_text, wraplength=400, justify='left')
-        tag_message.grid(row=0, column=1, sticky='w')
-
-        # Añadir el Frame al Canvas
-        canvas_frame.window_create(tk.END, window=frame)
-        canvas_frame.insert(tk.END, "\n")
-
-        # Actualizar la barra de desplazamiento
-        chat_scrollbar.config(command=canvas_frame.yview)
-        canvas_frame.config(yscrollcommand=chat_scrollbar.set)
-
-        # Actualizar la región de desplazamiento del Canvas
-        canvas_frame.update_idletasks()
-        canvas_frame.config(scrollregion=canvas_frame.bbox("all"))
-
-    except Exception as e:
-        logging.error("Error in the frame: %s", str(e))
-
 # --------------
 def send_message(chain):
     try:
@@ -109,11 +66,13 @@ def send_message(chain):
 
 def show_message(autor, mensaje, icon):
     try:
-        frame = tk.Frame(canvas_frame, bg=background_color)
-#        frame = tk.Frame(chat_history, bg=background_color)
-        #frame.pack(anchor='w', pady=5, padx=10)
+        frame = tk.Frame(chat_history, bg=background_color)
+        frame.pack(anchor='w', pady=5, padx=10)
         frame.grid_columnconfigure(0, weight=1)
         frame.grid_columnconfigure(1, weight=1)
+
+        v_scrollbar = ttk.Scrollbar(frame)
+        v_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         if icon:
             tag_icon = tk.Label(frame, image=icon, bg=background_color)
